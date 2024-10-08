84ee221bd5a60a123662fb2ceea4e0ba3c5e064f
adeirham <161985125+adeirham@users.noreply.github.com>
2024-10-08 23:01:02 +0700

    Add files via upload

---
+++ src/app/page.js
@@ -0,0 +1,19 @@
+import styles from "./styles/page.module.css"; 
+import './globals.css'; 
+
+export default function Home() {
+  return (
+    <div className="container"> 
+    <div className={styles.navbar}>
+      <div className={styles.navBar}>Navbar 1</div>
+      <div className={styles.navBar}>Navbar 2</div>
+      <div className={styles.navBar}>Navbar 3</div>
+
+    </div>
+      <div className={styles.sidebar}/> 
+      <div className="mainContent"> 
+      <div className={styles.KontenUtama}>KontenUtama</div>
+      </div>
+    </div>
+  );
+}
